16d5fbe5625c2424dd4b01d2824f903609cb5512
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-06 22:56:21 +0330

    Add files via upload

---
+++ ConsoleApp54/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Abs(-40));
+
+            Console.WriteLine(Math.Sqrt(9));
+
+            Console.WriteLine(Math.Max(4, 7));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
